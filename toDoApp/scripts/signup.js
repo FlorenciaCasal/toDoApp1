d4c8a3e44aaf9647a6a0c00b66603e68989706b4
FlorenciaCasal <florenciacasal88.fc@gmail.com>
2023-04-21 20:26:36 -0300

    hice varios cambios

--- toDoApp/scripts/signup.js
+++ toDoApp/scripts/signup.js
@@ -7,45 +7,63 @@ window.addEventListener('load', function () {
     const inputPassword = document.querySelector("#inputPassword")
     const inputPasswordRepetida = document.querySelector("#inputPasswordRepetida")
     const URL = "https://todo-api.ctd.academy/v1"
-    const path = "/users"   
-    const URI = URL + path 
+    const path = "/users"
+    const URI = URL + path
+    const tarjeta= document.getElementById("tarjetaError");
+
+
+    // Aquí en este punto yo me encargo de mandar a llamar las funciones de normalizar texto y validaciónes
+
+    inputNombre.addEventListener("blur", e => isEmpty(`⚠️ Se requiere que ingrese su ${inputNombre.name}`, e))
+    inputApellido.addEventListener("blur", e => isEmpty(`⚠️ Se requiere que ingrese su ${inputApellido.name}`, e))
+    inputEmail.addEventListener("blur", e => isEmpty(`⚠️ Se requiere que ingrese su ${inputEmail.name}`, e))
+    inputPassword.addEventListener("blur", e => isEmpty(`⚠️ Se requiere que ingrese su ${inputPassword.name}`, e))
+    inputPasswordRepetida.addEventListener("blur", e => isEmpty(`⚠️ Se requiere que ingrese su ${inputPassword.name}`, e))
+
+
+
+    inputEmail.addEventListener("input", validarEmail)
+    inputPassword.addEventListener("input", validarContrasenia)
+    //inputPasswordRepetida.addEventListener("input", compararContrasenias)
+
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-      event.preventDefault()
-    //   if (inputPassword !== inputPasswordRepetida) {
-    //     return {msg: "Las contraseñas no coinciden"}
-    //   }
-    //   Creamos el cuerpo de la request
-    const payload = {
-        firstName: inputNombre.value,
-        lastName: inputApellido.value,
-        email: inputEmail.value,
-        password: inputPassword.value
-    }
+        event.preventDefault()
+       
+        //   Creamos el cuerpo de la request
+        const payload = {
+            firstName: inputNombre.value,
+            lastName: inputApellido.value,
+            email: inputEmail.value,
+            password: inputPassword.value
+        }
+        // Configuramos la request del fetch
+        const settings = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify(payload)
+        }
 
-if(validarEmail(payload.email)===false) {
-        return;
-    }
 
+        if (payload.password !== inputPasswordRepetida.value) {
+           tarjeta.innerHTML= "Las contraseñas no coinciden, escribalas nuevamente"
+           tarjeta.classList.add('error')
+           return;
+        }
 
-    normalizarEmail(payload.email);
-    // Configuramos la request del fetch
-     const settings = {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify(payload)
-    }
-    // Lanzamos la consulta de loguin a la API
-    realizarRegister(settings)
+      // Lanzamos la consulta de loguin a la API
+      realizarRegister(settings)
 
-    // Limpio los campos del formulario
-    form.reset();
-    });
+      // Limpio los campos del formulario
+      form.reset();
+  });
+
+        
 
     /* -------------------------------------------------------------------------- */
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
@@ -57,7 +75,7 @@ if(validarEmail(payload.email)===false) {
                 // console.log(response);
                 return response.json()
             }) // response me devuelve el jwt
-            .then( resJSON => {
+            .then(resJSON => {
                 console.log(resJSON)
                 // console.log(responseJSON);
                 if (resJSON.jwt) {
--- toDoApp/scripts/utils.js
+++ toDoApp/scripts/utils.js
@@ -1,4 +1,4 @@
-const setErrors = (message, field, isError = true) => { 
+const setErrors = (message, field, isError = true) => {
     if (isError) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error")
@@ -8,25 +8,24 @@ const setErrors = (message, field, isError = true) => {
         field.nextElementSibling.classList.remove("error")
         field.nextElementSibling.innerText = ""
     }
- }
-
+}
 
 // valida si el input esta vacio
-const isEmpty = (message ,e) => { 
+const isEmpty = (message, e) => {
     const field = e.target
     const fieldValue = field.value
     console.log(field);
 
     if (fieldValue.length == 0) {
-        setErrors(message,field)
+        setErrors(message, field)
     } else {
         setErrors("", field, false)
     }
- }
+}
 
 /* ---------------------------------- texto --------------------------------- */
 function validarTexto(texto) {
-    
+
 }
 
 function normalizarTexto(texto) {
@@ -41,11 +40,11 @@ function validarEmail(e) {
 
     console.log(field);
 
-    if (fieldValue.length < 4 || !regex.test(fieldValue)) { // si el correo ingresado es mayor a 3 caracteres y supera el regex pongo el error
-        setErrors(`🚨 Por favor ingrese un ${field.name} válido`,field)
+    if (fieldValue.length < 4 || !regex.test(fieldValue)) { // si el correo ingresado es menor a 4 caracteres o supera el regex pongo el error
+        setErrors(`🚨 Por favor ingrese un ${field.name} válido`, field)
     } else {
         setErrors("", field, false)
-    }    
+    }
 }
 
 function normalizarEmail(email) {
@@ -56,19 +55,15 @@ function normalizarEmail(email) {
 function validarContrasenia(e) {
     const field = e.target
     const fieldValue = normalizarTexto(field.value)
-  //  const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 
     console.log(field);
 
-    // if (fieldValue.length > 3 && !regex.test(fieldValue)) {
     if (fieldValue.length < 4) {
-        setErrors("‼️ Por favor ingrese una contraseña válida",field)
+        setErrors("‼️ Por favor ingrese una contraseña válida", field)
     } else {
         setErrors("", field, false)
-    }    
+    }
 }
 
-function compararContrasenias(contrasenia_1, contrasenia_2) {
-    
-}
 
